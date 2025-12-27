827d2e9473481b690341fcdf7401c95007cc309b
kmedved <bibigon@gmail.com>
2025-12-27 23:09:16 +0800

    Add joint mutual information estimators

--- mrmr/__init__.py
+++ mrmr/__init__.py
@@ -2,6 +2,7 @@ from . import bigquery
 from . import pandas
 from . import polars
 from . import spark
+from . import mi
 from .pandas import mrmr_classif, mrmr_regression
 from .main import mrmr_base
 
