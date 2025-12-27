c873c808da8299174d1d2147bb6bee7434423587
kmedved <bibigon@gmail.com>
2025-12-27 23:09:29 +0800

    Add CEFS+ copula-based feature selection

--- mrmr/__init__.py
+++ mrmr/__init__.py
@@ -2,6 +2,7 @@ from . import bigquery
 from . import pandas
 from . import polars
 from . import spark
+from .cefsplus import cefsplus_regression
 from .pandas import mrmr_classif, mrmr_regression
 from .main import mrmr_base
 
